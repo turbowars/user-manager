59434dada8328f5522088e6a1c62cbdb40326e23
Dheeraj Sampath <dheeraj.sampath@nowfloats.com>
2017-03-20 21:31:45 +0530

    HTML validation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,34 +17,64 @@ $(function(){
  */
  
 
-//Create a user
+
+var context;
 
 //Read the list of users
-var context, xhr = new XMLHttpRequest();
-xhr.open('GET', 'data/users.json');
-xhr.onload = function() {
-    if (xhr.status === 200) {
-    	context = JSON.parse(xhr.responseText);
-        console.log('data: \n' + context);
-        bindData();
-    }
-    else {
-        alert('Request failed.  Returned status of ' + xhr.status);
-    }
-};
-xhr.send();
-
-function bindData() {
-
-//Binding data
-var source   = $("#user-template").html();
-var template = Handlebars.compile(source);
-var html    = template(context);
-$('.user-list tbody').append(html);
+var getUsers = function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data/users.json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            context = JSON.parse(xhr.responseText);
+            localStorage.setItem("data", xhr.responseText);
+            console.log('data: \n' + context);
+            bindData();
+        } else {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
 }
 
+//Create a user
+
 //Update the users
+var updateUsers = function(id) {
+
+
+}
 
 //Delete a user
+var deactivate = function(id) {
 
+    var xhr = new XMLHttpRequest(), updatedData;
+    xhr.open('PUT', 'data/users.json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var userData = JSON.parse(xhr.responseText);
+            for (i = 0; i < userData.length(); i++) {
+                if (userData[i].id == id) {
+                    alert('Om nama shivay!');
+                    updatedData = userData;
+                }
+            }
+        }
+    };
+    xhr.send(JSON.stringify(updatedData));
 
+}
+
+//Helper Functions
+
+var bindData = function() {
+
+    //Binding data
+    var source = $("#user-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(context);
+    $('.user-list tbody').append(html);
+}
+
+getUsers();
